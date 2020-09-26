f5fa0491087e87529c17383efce107bb49e09d65
highload-russia <radiodiversion@gmail.com>
2020-09-26 14:47:12 +0300

    * PropertiesProvider refactoring

--- src/main/java/com/highloadrussia/battleplanes/util/PropertiesProvider.java
+++ src/main/java/com/highloadrussia/battleplanes/util/PropertiesProvider.java
@@ -3,39 +3,33 @@ package com.highloadrussia.battleplanes.util;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Optional;
 import java.util.Properties;
 
 public class PropertiesProvider {
 
-    private static final Map<String, String> propertiesCache = new HashMap<>();
+    private static final String PROPERTIES_FILE_NAME = "application.properties";
+    private static final Properties appProps = new Properties();
+
+    static {
+        String rootPath = Optional.ofNullable(Thread.currentThread()
+                .getContextClassLoader()
+                .getResource("")
+        ).orElseThrow(() -> new RuntimeException("Cannot define path to game resources"))
+                .getPath();
+
+        try (InputStream input = new FileInputStream(rootPath + PROPERTIES_FILE_NAME)) {
+            appProps.load(input);
+        } catch (IOException e) {
+            throw new RuntimeException("Properties file reading error", e);
+        }
+    }
 
     public static int getIntValue(String propertyName) {
-        return Integer.parseInt(getStringValue(propertyName));
+        return Integer.parseInt(appProps.getProperty(propertyName));
     }
 
     public static String getStringValue(String propertyName) {
-
-        String propertyValue = propertiesCache.get(propertyName);
-
-        if (propertyValue != null) {
-            return propertyValue;
-        }
-
-        String rootPath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
-        String appConfigPath = rootPath + "application.properties";
-
-        Properties appProps = new Properties();
-
-        try (InputStream input = new FileInputStream(appConfigPath)) {
-            appProps.load(input);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        appProps.forEach((k, v) -> propertiesCache.put(k.toString(), v.toString()));
-
-        return propertiesCache.get(propertyName);
+        return appProps.getProperty(propertyName);
     }
 }
